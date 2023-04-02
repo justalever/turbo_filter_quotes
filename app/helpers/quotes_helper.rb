8efd035efd819da89e72cd230730772059893ee1
Andy Leverenz <andy@justalever.com>
2023-04-02 13:18:19 -0500

    Init

---
+++ app/helpers/quotes_helper.rb
@@ -0,0 +1,15 @@
+module QuotesHelper
+  def direction
+    params[:direction] == "asc" ? "desc" : "asc"
+  end
+
+  def filter_arrow(column)
+    if params[:column] == column
+      if params[:direction] == "asc"
+        "&#8659;".html_safe
+      else
+        "&#8657;".html_safe
+      end
+    end
+  end
+end
---
+++ db/migrate/20230402175709_create_quotes.rb
@@ -0,0 +1,11 @@
+class CreateQuotes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :quotes do |t|
+      t.string :quote
+      t.string :character
+      t.string :actor
+
+      t.timestamps
+    end
+  end
+end
